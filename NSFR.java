674cc69b01d7432a0f6fb36e1b3cef1e4c830298
reahoxha <rh49092@ubt-uni.net>
2021-12-11 16:24:08 +0100

    [shifting]

--- NSFR.java
+++ NSFR.java
@@ -7,30 +7,41 @@ public class NSFR{
     boolean [] key=new boolean[10];
     boolean [] iv=new boolean[10]; 
         init(iv);        
-        init(key);
+        //init(key);
     for(int j=0;j<10;j++){
         a[j]=iv[j];
         b[j]=key[j];
-        c[j]=key[j]^iv[j];
-        System.out.println(a[j]+" "+b[j]+" "+c[j]);
+        c[j]=iv[j]^key[j];
+        //System.out.println(a[j]+" "+b[j]+" "+c[j]);
     }
-    
+        shift(a);
     }
 public static boolean []init(boolean []k){   
     for(int i=0;i<k.length;i++){
+    int count=0;        
     String s=JOptionPane.showInputDialog("Shtyp 1 ose 0");
     int s1=Integer.valueOf(s);
-    int count=0;        
     if(s1==1){
         k[i]=true;
     }else if(s1==0){
         k[i]=false;
     }
     count=i+1;
-    System.out.print(k[i]+" "+count+ " ");
+    System.out.print(k[i]+" "+count+" ");
          }    
 
     return k;
-    }
 
+}
+public static boolean[] shift(boolean []a){
+    boolean last=a[a.length-1];
+    for(int s=a.length-2;s>=0;s--){
+    a[s+1]=a[s];
     }
+    a[0]=last;
+    for (int p=0;p<a.length;p++){
+        System.out.print(a[p]+" "+ p +" ");
+    }
+    return a;
+    }
+}
